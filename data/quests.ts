ca7c0e442a659102228e150035fa2f40589454e6
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-05 15:22:40 -0500

    Add the quests I can currently think of.

--- data/quests.ts
+++ data/quests.ts
@@ -4,4 +4,30 @@ export interface QuestType {
   requirements: Array<QuestType>;
 }
 
-export const Quests = [{}];
+export const Quests = [
+  {
+    id: 1,
+    name: "Ranni the Witch",
+    requirements: [],
+  },
+  {
+    id: 2,
+    name: "Roundtable",
+    requirements: [],
+  },
+  {
+    id: 3,
+    name: "Dung Eater",
+    requirements: [],
+  },
+  {
+    id: 4,
+    name: "Volcano Manor Assassinations",
+    requirements: [],
+  },
+  {
+    id: 5,
+    name: "Millicent",
+    requirements: [],
+  },
+];
