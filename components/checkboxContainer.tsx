e894d4f0eb51f7366a4113a67ad71065ac090fc3
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-07 15:04:51 -0500

    make a checkbox container

---
+++ components/checkboxContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {}
+
+const CheckboxContainer = (props: Props) => {
+  const {} = props;
+
+  return <fieldset></fieldset>;
+};
+
+export default CheckboxContainer;
