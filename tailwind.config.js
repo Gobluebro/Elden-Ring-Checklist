0fd6512b7504af8720f1bce45d9d3fe02ff2b57b
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-07 18:43:41 -0500

    Add base sizes for headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,5 +8,13 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.6xl") },
+        h2: { fontSize: theme("fontSize.5xl") },
+        h3: { fontSize: theme("fontSize.4xl") },
+      });
+    }),
+  ],
 };
