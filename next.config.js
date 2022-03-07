5235fd575a9e7d883d2d23b3b844491362f1a01f
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-07 18:49:52 -0500

    add base path and asset prefix to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const githubPath = "/Elden-Ring-Checklist";
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: githubPath,
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? githubPath : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
