d6210ff3f8dfe1e414c3d70b443c639dd4a10b43
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-07 22:29:21 -0500

    Add generic achievements list

---
+++ data/achievements.ts
@@ -0,0 +1,34 @@
+import { ListType } from "./types";
+
+export const Achievements: Array<ListType> = [
+  {
+    id: 1,
+    name: "",
+    requirements: [],
+  },
+  {
+    id: 2,
+    name: "",
+    requirements: [],
+  },
+  {
+    id: 3,
+    name: "",
+    requirements: [],
+  },
+  {
+    id: 4,
+    name: "",
+    requirements: [],
+  },
+  {
+    id: 5,
+    name: "",
+    requirements: [],
+  },
+  {
+    id: 6,
+    name: "",
+    requirements: [],
+  },
+];
