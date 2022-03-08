59ba92016e16a1727e2a1abaccb3b965773cbeca
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-07 19:14:41 -0500

    more generic name

--- components/checkboxContainer.tsx
+++ components/checkboxContainer.tsx
@@ -3,16 +3,16 @@ import { ListType } from "../data/types";
 import Checkbox from "./checkbox";
 
 interface Props {
-  quest: ListType;
+  list: ListType;
 }
 
 const CheckboxContainer = (props: Props) => {
-  const { quest } = props;
+  const { list } = props;
 
   return (
     <fieldset className="border border-solid border-gray-300 p-3">
-      <legend>{quest.name}</legend>
-      {quest.requirements.map((req) => (
+      <legend>{list.name}</legend>
+      {list.requirements.map((req) => (
         <Checkbox key={req.id} requirement={req} />
       ))}
     </fieldset>
