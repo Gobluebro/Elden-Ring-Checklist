d89e33e7179ceb1e3c9fc61dc87afbe03164d267
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-07 19:58:19 -0500

    Add the requirements for Ranni the Witch questline

--- data/quests.ts
+++ data/quests.ts
@@ -7,7 +7,92 @@ export const Quests: Array<ListType> = [
     requirements: [
       {
         id: 1,
-        description: "",
+        description: "Talk to Ranni in Ranni's Rise.",
+      },
+      {
+        id: 2,
+        description:
+          "Talk to Seluvis, Iji, and Blaidd down the stairs of Ranni's Rise.",
+      },
+      {
+        id: 3,
+        description: "Talk to Blaidd in the Siofra River.",
+      },
+      {
+        id: 4,
+        description: "Talk to Seluvis in Seluvis's Rise.",
+      },
+      {
+        id: 5,
+        description: "Talk to Sellen at the Waypoint Ruins Cellar grace.",
+      },
+      {
+        id: 6,
+        description: "Talk to Blaidd in the Siofra River.",
+      },
+      {
+        id: 7,
+        description: "Defeat Radahn.",
+      },
+      {
+        id: 8,
+        description: "Get Finger Slayer Blade in Nokron.",
+      },
+      {
+        id: 9,
+        description:
+          "Give the item to Ranni in Ranni's Rise. You will receive the inverted statue",
+      },
+      {
+        id: 10,
+        description:
+          "Take the inverted statue to the study hall entrance grace and place it on the table.",
+      },
+      {
+        id: 11,
+        description:
+          "Get the cursemark of death from the Divine Tower of Liurnia.",
+      },
+      {
+        id: 12,
+        description:
+          "Go to Renna's Rise (not Ranni's Rise) and enter the the portal.",
+      },
+      {
+        id: 13,
+        description: "Pick up the Miniature Ranni.",
+      },
+      {
+        id: 14,
+        description:
+          "Rest at the nearby grace and 'Talk to the miniature Ranni' 3 times.",
+      },
+      {
+        id: 15,
+        description:
+          "Get the Discarded Palace Key by defeating the Baleful Shadow in Nokstella. Make sure to get the Lake of Rot Shoreside grace after.",
+      },
+      {
+        id: 15,
+        description:
+          "Get the Dark Moon Ring from the chest at the Raya Lucaria Grand Library by using the Discarded Palace Key.",
+      },
+      {
+        id: 16,
+        description: "Defeat Astel, Naturalborn of the Void.",
+      },
+      {
+        id: 17,
+        description:
+          "Put the Dark Moon Ring on Ranni's Finger below the Cathedral of Manus Celes.",
+      },
+      {
+        id: 18,
+        description: "Defeat Blaidd outside of Ranni's Rise.",
+      },
+      {
+        id: 19,
+        description: "Talk to Iji about Blaidd's death.",
       },
     ],
   },
