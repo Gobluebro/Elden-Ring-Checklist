8e91664e1ed53a3a23eb00c07b3bbad0d99e6392
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-09 16:18:56 -0500

    Add graces based on how I currently see them in the game.

---
+++ data/graces.ts
@@ -0,0 +1,172 @@
+export const Graces = [
+  {
+    id: 1,
+    name: "Roundtable Hold",
+    requirements: [],
+  },
+  {
+    id: 2,
+    name: "Limgrave",
+    requirements: [],
+  },
+  {
+    id: 3,
+    name: "Stranded Graveyard",
+    requirements: [],
+  },
+  {
+    id: 4,
+    name: "Stormhill",
+    requirements: [],
+  },
+  {
+    id: 5,
+    name: "Weeping Peninsula",
+    requirements: [],
+  },
+  {
+    id: 6,
+    name: "Stormveil Castle",
+    requirements: [],
+  },
+  {
+    id: 7,
+    name: "Liurnia of the Lakes",
+    requirements: [],
+  },
+  {
+    id: 8,
+    name: "Bellum Highway",
+    requirements: [],
+  },
+  {
+    id: 9,
+    name: "Ruin-Strewn Precipe",
+    requirements: [],
+  },
+  {
+    id: 10,
+    name: "Moonlight Altar",
+    requirements: [],
+  },
+  {
+    id: 11,
+    name: "Academy of Raya Lucaria",
+    requirements: [],
+  },
+  {
+    id: 12,
+    name: "Altus Plateau",
+    requirements: [],
+  },
+  {
+    id: 13,
+    name: "Mt. Gelmir",
+    requirements: [],
+  },
+  {
+    id: 14,
+    name: "Leyndell, Royal Capital",
+    requirements: [],
+  },
+  {
+    id: 15,
+    name: "Volcano Manor",
+    requirements: [],
+  },
+  {
+    id: 16,
+    name: "Subterranean Shunning-Grounds",
+    requirements: [],
+  },
+  {
+    id: 17,
+    name: "Leyndell Ashen Capital",
+    requirements: [],
+  },
+  {
+    id: 18,
+    name: "Caelid",
+    requirements: [],
+  },
+  {
+    id: 19,
+    name: "Swamp of Aeonia",
+    requirements: [],
+  },
+  {
+    id: 20,
+    name: "Greyoll's Dragonbarrow",
+    requirements: [],
+  },
+  {
+    id: 21,
+    name: "Forbidden Lands",
+    requirements: [],
+  },
+  {
+    id: 22,
+    name: "Mountaintops of the Giants",
+    requirements: [],
+  },
+  {
+    id: 23,
+    name: "Flame Peak",
+    requirements: [],
+  },
+  {
+    id: 24,
+    name: "Consecrated Snowfield",
+    requirements: [],
+  },
+  {
+    id: 25,
+    name: "Miquella's Haligtree",
+    requirements: [],
+  },
+  {
+    id: 26,
+    name: "Elphael, Brace of the Haligtree",
+    requirements: [],
+  },
+  {
+    id: 27,
+    name: "Ainsel River",
+    requirements: [],
+  },
+  {
+    id: 28,
+    name: "Ainsel River Main",
+    requirements: [],
+  },
+  {
+    id: 29,
+    name: "Lake of Rot",
+    requirements: [],
+  },
+  {
+    id: 30,
+    name: "Nokron, Eternal City",
+    requirements: [],
+  },
+  {
+    id: 31,
+    name: "Siofra River",
+    requirements: [],
+  },
+  {
+    id: 32,
+    name: "Mohgwyn Palace",
+    requirements: [],
+  },
+  {
+    id: 33,
+    name: "Deeproot Depths",
+    requirements: [],
+  },
+  {
+    id: 34,
+    name: "Crumbling Farum Azula",
+    requirements: [],
+  },
+];
