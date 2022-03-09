d37ff3ad5d7a7c20f82e7b92fc3756316f10463b
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-09 16:47:37 -0500

    Add graces from scrapped data

--- data/graces.ts
+++ data/graces.ts
@@ -1,172 +1,725 @@
-export const Graces = [
+import { ListType } from "./types";
+
+export const Graces: Array<ListType> = [
   {
     id: 1,
     name: "Roundtable Hold",
-    requirements: [],
+    requirements: [{ id: 1, description: "Table of Lost Grace" }],
   },
   {
     id: 2,
     name: "Limgrave",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "The First Step",
+      },
+      {
+        id: 2,
+        description: "Church of Elleh",
+      },
+      {
+        id: 3,
+        description: "Gatefront",
+      },
+      {
+        id: 4,
+        description: "Waypoint Ruins Cellar",
+      },
+      {
+        id: 5,
+        description: "Artist's Shack",
+      },
+      {
+        id: 6,
+        description: "Third Church of Marika",
+      },
+      {
+        id: 7,
+        description: "Fort Haight West",
+      },
+      {
+        id: 8,
+        description: "Agheel Lake South",
+      },
+      {
+        id: 9,
+        description: "Agheel Lake North",
+      },
+      {
+        id: 10,
+        description: "Church of Dragon Communion",
+      },
+      {
+        id: 11,
+        description: "Seaside Ruins",
+      },
+      {
+        id: 12,
+        description: "Mistwood Outskirts",
+      },
+      {
+        id: 13,
+        description: "Murkwater Coast",
+      },
+      {
+        id: 14,
+        description: "Summonwater Village Outskirts",
+      },
+      {
+        id: 15,
+        description: "Stormfoot Catacombs",
+      },
+      {
+        id: 16,
+        description: "Murkwater Catacombs",
+      },
+      {
+        id: 17,
+        description: "Groveside Cave",
+      },
+      {
+        id: 18,
+        description: "Coastal Cave",
+      },
+      {
+        id: 19,
+        description: "Murkwater Cave",
+      },
+      {
+        id: 20,
+        description: "Highroad Cave",
+      },
+      {
+        id: 21,
+        description: "Limgrave Tunnels",
+      },
+    ],
   },
   {
     id: 3,
     name: "Stranded Graveyard",
-    requirements: [],
+    requirements: [
+      { id: 1, description: "Cave of Knowledge" },
+      { id: 2, description: "Stranded Graveyard" },
+    ],
   },
   {
     id: 4,
     name: "Stormhill",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "Stormhill Shack",
+      },
+      {
+        id: 2,
+        description: "Castleward Tunnel",
+      },
+      {
+        id: 3,
+        description: "Margit, the Fell Omen",
+      },
+      {
+        id: 4,
+        description: "Warmaster's Shack",
+      },
+      {
+        id: 5,
+        description: "Saintsbridge",
+      },
+      {
+        id: 6,
+        description: "Deathtouched Catacombs",
+      },
+      {
+        id: 7,
+        description: "Limgrave Tower Bridge",
+      },
+      {
+        id: 8,
+        description: "Divine Tower of Limgrave",
+      },
+    ],
   },
   {
     id: 5,
     name: "Weeping Peninsula",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "Church of Pilgrimage",
+      },
+      {
+        id: 2,
+        description: "Castle Morne Rampart",
+      },
+      {
+        id: 3,
+        description: "Tombsward",
+      },
+      {
+        id: 4,
+        description: "South of the Lookout Tower",
+      },
+      {
+        id: 5,
+        description: "Ailing Village Outskirts",
+      },
+      {
+        id: 6,
+        description: "Beside the Crater-Pocked Glade",
+      },
+      {
+        id: 7,
+        description: "Isolated Merchant's Shack",
+      },
+      {
+        id: 8,
+        description: "Fourth Church of Marika",
+      },
+      {
+        id: 9,
+        description: "Bridge of Sacrifice",
+      },
+      {
+        id: 10,
+        description: "Castle Morne Lift",
+      },
+      {
+        id: 11,
+        description: "Behind the Castle",
+      },
+      {
+        id: 12,
+        description: "Beside the Rampart Gaol",
+      },
+      {
+        id: 13,
+        description: "Morne Moangrave",
+      },
+      {
+        id: 14,
+        description: "Impaler's Catacombs",
+      },
+      {
+        id: 15,
+        description: "Tombsward Catacombs",
+      },
+      {
+        id: 16,
+        description: "Earthbore Cave",
+      },
+      {
+        id: 17,
+        description: "Tombsward Cave",
+      },
+      {
+        id: 18,
+        description: "Morne Tunnel",
+      },
+    ],
   },
   {
     id: 6,
     name: "Stormveil Castle",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "Stormveil Main Gate",
+      },
+      {
+        id: 2,
+        description: "Gateside Chamber",
+      },
+      {
+        id: 3,
+        description: "Stormveil Cliffside",
+      },
+      {
+        id: 4,
+        description: "Rampart Tower",
+      },
+      {
+        id: 5,
+        description: "Liftside Chamber",
+      },
+      {
+        id: 6,
+        description: "Secluded Cell",
+      },
+      {
+        id: 7,
+        description: "Godrick the Grafted",
+      },
+    ],
   },
   {
     id: 7,
     name: "Liurnia of the Lakes",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "Lake-Facing Cliffs",
+      },
+      {
+        id: 2,
+        description: "Liurnia Lake Shore",
+      },
+      {
+        id: 3,
+        description: "Laskyar Ruins",
+      },
+      {
+        id: 4,
+        description: "Scenic Isle",
+      },
+      {
+        id: 5,
+        description: "Academy Gate Town",
+      },
+      {
+        id: 6,
+        description: "South Raya Lucaria Gate",
+      },
+      {
+        id: 7,
+        description: "Main Academy Gate",
+      },
+      {
+        id: 8,
+        description: "Liurnia Highway South",
+      },
+      {
+        id: 9,
+        description: "Liurnia Highway North",
+      },
+      {
+        id: 10,
+        description: "Gate Town Bridge",
+      },
+      {
+        id: 11,
+        description: "Artist's Shack",
+      },
+      {
+        id: 12,
+        description: "Eastern Liurnia Lake Shore",
+      },
+      {
+        id: 13,
+        description: "Eastern Tableland",
+      },
+      {
+        id: 14,
+        description: "Church of Vows",
+      },
+      {
+        id: 15,
+        description: "Ruined Labyrinth",
+      },
+      {
+        id: 16,
+        description: "Boilprawn Shack",
+      },
+      {
+        id: 17,
+        description: "Folly on the Lake",
+      },
+      {
+        id: 18,
+        description: "Slumbering Wolf's Shack",
+      },
+      {
+        id: 19,
+        description: "Village of the Albinaurics",
+      },
+      {
+        id: 20,
+        description: "Converted Tower",
+      },
+      {
+        id: 21,
+        description: "Revenger's Shack",
+      },
+      {
+        id: 22,
+        description: "Temple Quarter",
+      },
+      {
+        id: 23,
+        description: "Crystalline Woods",
+      },
+      {
+        id: 24,
+        description: "Foot of the Four Belfries",
+      },
+      {
+        id: 25,
+        description: "The Four Belfries",
+      },
+      {
+        id: 26,
+        description: "Sorcerer's Isle",
+      },
+      {
+        id: 27,
+        description: "Gate Town North",
+      },
+      {
+        id: 28,
+        description: "Northern Liurnia Lake Shore",
+      },
+      {
+        id: 29,
+        description: "Road to the Manor",
+      },
+      {
+        id: 30,
+        description: "Main Caria Manor Gate",
+      },
+      {
+        id: 31,
+        description: "Manor Upper Level",
+      },
+      {
+        id: 32,
+        description: "Manor Lower Level",
+      },
+      {
+        id: 33,
+        description: "Royal Moongazing Grounds",
+      },
+      {
+        id: 34,
+        description: "Ranni's Rise",
+      },
+      {
+        id: 35,
+        description: "Behind Caria Manor",
+      },
+      {
+        id: 36,
+        description: "The Ravine",
+      },
+      {
+        id: 37,
+        description: "Ravine-Veiled Village",
+      },
+      {
+        id: 38,
+        description: "Road's End Catacombs",
+      },
+      {
+        id: 39,
+        description: "Black Knife Catacombs",
+      },
+      {
+        id: 40,
+        description: "Stillwater Cave",
+      },
+      {
+        id: 41,
+        description: "Lakeside Crystal Cave",
+      },
+      {
+        id: 42,
+        description: "Raya Lucaria Crystal Tunnel",
+      },
+      {
+        id: 43,
+        description: "Academy Crystal Cave",
+      },
+      {
+        id: 44,
+        description: "Study Hall Entrance",
+      },
+    ],
   },
   {
     id: 8,
     name: "Bellum Highway",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "East Raya Lucaria Gate",
+      },
+      {
+        id: 2,
+        description: "Bellum Church",
+      },
+      {
+        id: 3,
+        description: "Grand Lift of Dectus",
+      },
+    ],
   },
   {
     id: 9,
     name: "Ruin-Strewn Precipe",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "Ruin-Strewn Precipice",
+      },
+      {
+        id: 2,
+        description: "Ruin-Strewn Precipice Overlook",
+      },
+      {
+        id: 3,
+        description: "Magma Wyrm",
+      },
+    ],
   },
   {
     id: 10,
     name: "Moonlight Altar",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 11,
     name: "Academy of Raya Lucaria",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 12,
     name: "Altus Plateau",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 13,
     name: "Mt. Gelmir",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 14,
     name: "Leyndell, Royal Capital",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 15,
     name: "Volcano Manor",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 16,
     name: "Subterranean Shunning-Grounds",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 17,
     name: "Leyndell Ashen Capital",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 18,
     name: "Caelid",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 19,
     name: "Swamp of Aeonia",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 20,
     name: "Greyoll's Dragonbarrow",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 21,
     name: "Forbidden Lands",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 22,
     name: "Mountaintops of the Giants",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 23,
     name: "Flame Peak",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 24,
     name: "Consecrated Snowfield",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 25,
     name: "Miquella's Haligtree",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 26,
     name: "Elphael, Brace of the Haligtree",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 27,
     name: "Ainsel River",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 28,
     name: "Ainsel River Main",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 29,
     name: "Lake of Rot",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 30,
     name: "Nokron, Eternal City",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 31,
     name: "Siofra River",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 32,
     name: "Mohgwyn Palace",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 33,
     name: "Deeproot Depths",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
   {
     id: 34,
     name: "Crumbling Farum Azula",
-    requirements: [],
+    requirements: [
+      {
+        id: 1,
+        description: "",
+      },
+    ],
   },
 ];
