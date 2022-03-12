f0607a2013f2f5917407d223c388c4bf5d7bad10
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-12 18:31:37 -0500

    add some wording for reporting an issue

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="flex-1 flex flex-col justify-end items-center mb-8">
       <span>
-        Check out the project on{" "}
+        Check out the project or report an issue on{" "}
         <a
           className="text-elden-ring-green dark:text-elden-ring-gold font-bold hover:underline"
           href="https://github.com/Gobluebro/Elden-Ring-Checklist"
