7c422ad8c583dba87ea6e6745f070991a1516ea2
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-14 02:05:36 -0400

    Add new colors and packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        "elden-ring-green": {
+          0: "#ebfff4",
+          50: "#d3f3e2",
+          100: "#bce8d1",
+          150: "#a7dcc1",
+          200: "#93d0b1",
+          300: "#71b996",
+          400: "#56a17f",
+          500: "#418a69",
+          600: "#317256",
+          700: "#235b43",
+          800: "#194331",
+          900: "#0f2c20",
+          1000: "#07140f",
+        },
         "elden-ring-dark-blue": "#0c2527",
         "elden-ring-dark-green": "#14302d",
-        "elden-ring-green": "#3a5950",
         "elden-ring-light-green": "#3a5950",
         "elden-ring-gold": "#c39e66",
       },
@@ -25,5 +39,6 @@ module.exports = {
         h3: { fontSize: theme("fontSize.4xl") },
       });
     }),
+    require("@tailwindcss/forms"),
   ],
 };
