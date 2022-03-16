dde37c51b1e77e974b7c2ebc6b8746f21a05cc3b
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-03-16 05:50:57 -0400

    Start of bosses data

---
+++ data/bosses.ts
@@ -0,0 +1,367 @@
+import { ListType } from "./types";
+[
+  "Battlemage Hugues",
+  "Bell Bearing Hunter",
+  "Black Blade Kindred (Caelid)",
+  "Cemetery Shade",
+  "Cleanrot Knight",
+  "Commander O'Neil",
+  "Death Rite Bird",
+  "Decaying Ekzykes",
+  "Elder Dragon Greyoll",
+  "Erdtree Avatar (Caelid)",
+  "Erdtree Burial Watchdog (Minor Erd)",
+  "Fallingstar Beast",
+  "Flying Dragon Greyll",
+  " Frenzied Duelist",
+  "Godskin Apostle",
+  "Mad Pumpkin Heads",
+  "Magma Wyrm",
+  "Night's Cavalry",
+  "Nox Swordstress & Nox Priest",
+  "Putrid Avatar",
+  "Putrid Tree Spirit",
+  "Putrid Crystallian Trio",
+  "Starscourge Radahn",
+];
+
+export const Bosses: Array<ListType> = [
+  {
+    id: "6578f21c-b712-4dda-bbb4-969fab04193b",
+    name: "Limgrave",
+    requirements: [
+      {
+        id: "66fbe453-878b-4661-9aee-269941290ac0",
+        description: "Ancient Hero of Zamor",
+      },
+      {
+        id: "a0bd6291-7b69-4ff1-9f0a-ea1e7db313dc",
+        description: "Beast of Farum Azula",
+      },
+      {
+        id: "ed0361c9-1386-46dc-a5bf-53e541af28ce",
+        description: "Bell Bearing Hunter",
+      },
+      {
+        id: "b71415f3-2777-4c5e-a3f4-406d0e1cf909",
+        description: "Black Knife Assassin",
+      },
+      {
+        id: "d5f90987-572c-4ec3-b79f-bce5166c33a6",
+        description: "Bloodhound Knight Darriwil",
+      },
+      {
+        id: "9e0838f0-9580-4754-9b32-687646b748fb",
+        description: "Cemetery Shade",
+      },
+      {
+        id: "4e0786b6-9127-4a91-b7e1-3553229d47ab",
+        description: "Crucible Knight",
+      },
+      {
+        id: "b7f6e298-7c24-417b-a69a-ce02ee241b0a",
+        description: "Death Bird",
+      },
+      {
+        id: "0642b0a6-9f3c-42a1-9b62-1b31bbc7db8b",
+        description: "Demi-Human Chief",
+      },
+      {
+        id: "080773ad-c7e7-4628-9d41-aa267312da45",
+        description: "Erdtree Avatar",
+      },
+      {
+        id: "b092b7ff-f4a4-4580-a491-66e8d5497768",
+        description: "Erdtree Burial Watchdog (Stormfoot)",
+      },
+      {
+        id: "6661ea64-18aa-42f1-b26e-a8f30436171e",
+        description: "Erdtree Burial Watchdog (Impaler)",
+      },
+      {
+        id: "53a48aff-a381-4c6e-8153-c4108fa2ccfa",
+        description: "Flying Dragon Agheel",
+      },
+      {
+        id: "53aceebc-d34b-4103-b72b-b78e8c3d7937",
+        description: "Godrick the Grafted",
+      },
+      {
+        id: "6a4f7913-3ced-4eb0-b660-61c45e635fa3",
+        description: "Grafted Scion",
+      },
+      {
+        id: "0b351ef8-1104-4d40-ab64-e47304ac7f0a",
+        description: "Grave Warden Duelist",
+      },
+      {
+        id: "6f60e7ff-d635-466d-96e4-05ff97f5c593",
+        description: "Guardian Golem",
+      },
+      {
+        id: "71f7c8e9-ae00-47ff-b317-9c75445a3c87",
+        description: "Leonine Misbegotten",
+      },
+      {
+        id: "1e94b0d3-5f03-4cee-9867-4cb5f5ca27f6",
+        description: "Lion Guardian",
+      },
+      {
+        id: "ce9d9e01-1849-4bb4-86a6-dd72ab62a1fd",
+        description: "Mad Pumpkin Head",
+      },
+      {
+        id: "ec5162d7-4910-49ed-bf7c-aea7d73425c2",
+        description: "Margit, the Fell Omen",
+      },
+      {
+        id: "d0b27951-3dd3-4cdd-8cc4-ec8a6aa7c850",
+        description: "Miranda the Blighted Bloom",
+      },
+      {
+        id: "f6e9b196-fca8-449f-88f5-8f22df66f9ec",
+        description: "Night's Cavalry",
+      },
+      {
+        id: "05c6ccb5-cd55-45a2-bdd9-8b650c265b75",
+        description: "Patches",
+      },
+      {
+        id: "9365bae2-230e-4d39-b4e8-6c1cd5f434a2",
+        description: "Runebear",
+      },
+      {
+        id: "d6b1e699-f8f5-4cb9-bd03-04e7461428fa",
+        description: "Scaly Misbegotten",
+      },
+      {
+        id: "b8394a3c-03ab-491e-be5a-77926c8f561c",
+        description: "Soldier of Godrick",
+      },
+      {
+        id: "f85ac6de-6b4c-4af4-92ca-ec292fea1c5a",
+        description: "Stonedigger Troll",
+      },
+      {
+        id: "39b8c4c2-0ee2-4307-9a28-cd6692076f6d",
+        description: "Tibia Mariner",
+      },
+      {
+        id: "8d8490ed-de14-4baf-9c01-6848b671cffe",
+        description: "Tree Sentinel",
+      },
+      {
+        id: "5500d9ee-9a32-499f-83f2-6bc92baa129f",
+        description: "Ulcerated Tree Spirit",
+      },
+    ],
+  },
+  {
+    id: "cee4d03a-9527-434a-8c6b-eb2bb1e555e3",
+    name: "Liurnia of the Lakes",
+    requirements: [
+      {
+        id: "81eedf79-718f-4357-a93b-4a4d8c06f7a4",
+        description: "Adan, Thief of Fire",
+      },
+      {
+        id: "b08f4c29-6b2a-40a9-8369-8ff4aea3ef2a",
+        description: "Alabaster Lord",
+      },
+      {
+        id: "8538dca5-70e6-409f-8e5e-ed52e07d62fc",
+        description: "Alecto, Black Knife Ringleader",
+      },
+      {
+        id: "655d9ed9-96a5-4f91-a7b9-d6b55bd59f63",
+        description: "Bell Bearing Hunter",
+      },
+      {
+        id: "7e09323e-51c4-4c2f-b4af-7454f3e90699",
+        description: "Black Knife Assassin",
+      },
+      {
+        id: "0d3939e7-35d8-4feb-ae9d-ebc77c9d2c60",
+        description: "Bols, Carian Knight",
+      },
+      {
+        id: "28147b6f-5266-4e5d-b386-8946ea57ded7",
+        description: "Cemetery Shade",
+      },
+      {
+        id: "ada7ab11-711e-427a-ae42-bad6df89e659",
+        description: "Cleanrot Knight",
+      },
+      {
+        id: "001aa3b5-61e4-4c01-94e7-a5c8c3966472",
+        description: "Crystalian Spear & Crystalian Staff",
+      },
+      {
+        id: "f832fc0f-026b-46ac-9ec9-731dd35ed8f0",
+        description: "Crystalian Ringblade",
+      },
+      {
+        id: "19f4ceb0-05f3-4fb9-b6af-e55aafa87ef3",
+        description: "Death Bird",
+      },
+      {
+        id: "ebc76672-6e71-48f8-8873-118597ce07ef",
+        description: "Erdtree Avatar (Northeast)",
+      },
+      {
+        id: "9d0ff066-cc8a-45a9-bc1f-8b02aff8ff94",
+        description: "Erdtree Avatar (Southwest)",
+      },
+      {
+        id: "58414ef1-3ddc-4de9-b9e4-f4406e3e1ad8",
+        description: "Erdtree Burial Watchdog",
+      },
+      {
+        id: "4eb0be15-4127-48eb-bc72-9747d26baa89",
+        description: "Glintstone Dragon Adula",
+      },
+      {
+        id: "243c3e65-e3d9-4075-bdd0-8868b6b2f363",
+        description: "Glintstone Dragon Smarag",
+      },
+      {
+        id: "c82804c5-a671-43aa-b533-0cd9da55bca6",
+        description: "Godskin Noble",
+      },
+      {
+        id: "0454e11e-4a7c-4fb6-80e5-59d81573ade5",
+        description: "Magma Wyrm Makar",
+      },
+      {
+        id: "165c520f-8c79-4936-a284-141efff498c0",
+        description: "Night's Cavalry",
+      },
+      {
+        id: "0fe483c8-c471-442f-91da-86ab3b0f7181",
+        description: "Omenkiller",
+      },
+      {
+        id: "40992ac5-c3af-4590-8d3d-0a1fd22b1417",
+        description: "Red Wolf of Radagon",
+      },
+      {
+        id: "bda02e48-ce46-4c83-9097-1979a900b88a",
+        description: "Rennala, Queen of the Full Moon",
+      },
+      {
+        id: "b0c71e6b-90d8-4ff2-9749-5aa921c1b298",
+        description: "Royal Knight Loretta",
+      },
+      {
+        id: "b51aea6d-dcd8-4c9d-a269-3ff15e72c8a5",
+        description: "Royal Revenant",
+      },
+      {
+        id: "c47844bb-d643-473b-9ba2-730cc51d91f6",
+        description: "Spirit-Caller Snail",
+      },
+      {
+        id: "fbcea982-493e-4abd-92c8-350f4f38e546",
+        description: "Tibia Mariner",
+      },
+    ],
+  },
+  {
+    id: "22dceb87-f6c0-46c7-8405-86f352112dc1",
+    name: "Caelid",
+    requirements: [
+      {
+        id: "c762244f-c9ac-4bd7-8a62-a17aa0c00866",
+        description: "Battlemage Hugues",
+      },
+      {
+        id: "69692edc-8f0d-4b36-9e8f-620be77e1d26",
+        description: "Bell Bearing Hunter",
+      },
+      {
+        id: "d3e29140-9bff-44dd-bf58-6fdf1b878f63",
+        description: "Black Blade Kindred",
+      },
+      {
+        id: "c46e318e-cb38-4213-a70b-1de1c14699f4",
+        description: "Cemetery Shade",
+      },
+      {
+        id: "22d2dc53-0d13-45d8-bbd0-30ea7f0cdb89",
+        description: "Cleanrot Knight",
+      },
+      {
+        id: "6a4d583a-7ca8-48ce-90b8-339abfcab3d8",
+        description: "Commander O'Neil",
+      },
+      {
+        id: "3692d230-1d92-4112-91a1-4c71b244dc06",
+        description: "Death Rite Bird",
+      },
+      {
+        id: "52db4991-b8a4-4c6d-b8cc-a641c433789b",
+        description: "Decaying Ekzykes",
+      },
+      {
+        id: "9990cdfd-83a4-4421-a4d3-2b8c4e793552",
+        description: "Elder Dragon Greyoll",
+      },
+      {
+        id: "eff22d2f-bbdd-43a4-b17f-eee6d3f245e9",
+        description: "Erdtree Avatar",
+      },
+      {
+        id: "3f91f4a0-2849-451e-b5ef-26e3bdbe981e",
+        description: "Erdtree Burial Watchdog",
+      },
+      {
+        id: "18943d19-10e3-462d-89ff-3bd32ab0b17d",
+        description: "Fallingstar Beast",
+      },
+      {
+        id: "7d2c17b4-49d8-4b4e-9bab-f4cef39cd315",
+        description: "Flying Dragon Greyll",
+      },
+      {
+        id: "fb7631ad-745a-4a83-8c54-949f086c3ee4",
+        description: " Frenzied Duelist",
+      },
+      {
+        id: "9a947577-a0d7-4de1-94e5-3e4842c513f9",
+        description: "Godskin Apostle",
+      },
+      {
+        id: "f9060a52-c138-46c7-917c-28cd6bebd018",
+        description: "Mad Pumpkin Heads",
+      },
+      {
+        id: "a0d6d196-5711-466c-b906-0c6a6b8656c2",
+        description: "Magma Wyrm",
+      },
+      {
+        id: "468bc882-7a3a-4b3a-895c-e65c7c3ffc03",
+        description: "Night's Cavalry",
+      },
+      {
+        id: "c45cd445-fcd0-40cb-b3df-0bf07100a02c",
+        description: "Nox Swordstress & Nox Priest",
+      },
+      {
+        id: "fa9e4845-23a1-4ef2-a0f5-7d187f12b828",
+        description: "Putrid Avatar",
+      },
+      {
+        id: "d4b0d8ab-f894-4848-ae03-9ea4d1ff4a9a",
+        description: "Putrid Tree Spirit",
+      },
+      {
+        id: "54672242-59e6-4b68-90be-08e212d48a0c",
+        description: "Putrid Crystallian Trio",
+      },
+      {
+        id: "292b242a-6580-403f-a265-024e80f4636d",
+        description: "Starscourge Radahn",
+      },
+    ],
+  },
+];
