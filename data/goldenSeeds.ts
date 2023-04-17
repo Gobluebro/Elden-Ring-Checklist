15888d8ffdc55907710a264be0d7d64faedf1b4b
Roland Doll <rdoll007@gmail.com>
2023-04-17 14:20:20 -0400

    limgrave

--- data/goldenSeeds.ts
+++ data/goldenSeeds.ts
@@ -1,5 +1,35 @@
 import { ListType } from "./types";
 
 export const GoldenSeeds: Array<ListType> = [
+    {
+        id: "91bf1b6f-4012-4e62-abe0-c9ff1b9aea9d",
+        name: "Limgrave",
+        requirements: [
+            {
+                id: "72013d37-b80f-4690-806f-a71f219b063c",
+                description: "Stormhill: On the path from Stormgate to Stormhill Shack"
+            },
+            {
+                id: "1af082a1-cd98-4456-9a2e-f88509173094",
+                description: "Stormhill Shack: Where Roderika was once the player rests in Liurnia or gives her Chrysalids' Memento"
+            },
+            {
+                id: "0a5107da-a70e-42ea-be73-eb3540558d37",
+                description: "Mistwood: In front of the entrance to Fort Haight"
+            },
+            {
+                id: "1a29d16b-0045-4b0e-ac0e-be89f01491e6",
+                description: "Weeping Peninsula: Under a phantom tree on a ridge along the road to Castle Morne"
+            },
+            {
+                id: "6895be25-269c-4616-b3cd-770e9a506a69",
+                description: "Dropped from Kenneth Haight when killed (Missable once Kenneth moves to the Stormveil Throne Room as he cannot be killed there. There are more than enough Golden Seeds to skip this one if players aren't inclined to kill NPCs)"
+            },
+            {
+                id: "f7b3df16-341a-4fe7-b1b0-ac8f2a76d7fe",
+                description: "Fringefolk Hero's Grave: Defeat the Ulcerated Tree Spirit"
+            },
+        ]
+    }
 
 ]
