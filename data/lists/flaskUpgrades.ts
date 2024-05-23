9c8801792689cb0ccbe595c24aa34fb1856dd84e
Griffin Strayer <14102413+Gobluebro@users.noreply.github.com>
2024-05-23 12:38:26 -0400

    Add sacred tears data

--- data/lists/flaskUpgrades.ts
+++ data/lists/flaskUpgrades.ts
@@ -1,13 +1,13 @@
 import { ListType } from "./types";
 
-export const GoldenSeedsList: Array<ListType> = [
+export const FlaskUpgradesList: Array<ListType> = [
   {
     id: "f706904c-d313-4551-b346-d1e72afd04fa",
     name: "Character Creation",
     requirements: [
       {
         id: "6fdb9041-88be-43ed-8789-a08e4eb72934",
-        description: "Optional: Select as a Keepsake",
+        description: "Golden Seed - Optional: Select as a Keepsake",
       },
     ],
   }, // note: 30 seeds = max amount per play-through.
@@ -17,31 +17,55 @@ export const GoldenSeedsList: Array<ListType> = [
     requirements: [
       {
         id: "72013d37-b80f-4690-806f-a71f219b063c",
-        description: "Stormhill: On the path from Stormgate to Stormhill Shack",
+        description:
+          "Golden Seed - Stormhill: On the path from Stormgate to Stormhill Shack",
       },
       {
         id: "1af082a1-cd98-4456-9a2e-f88509173094",
         description:
-          "Stormhill Shack: Where Roderika was once the player rests in Liurnia or gives her Chrysalids' Memento",
+          "Golden Seed - Stormhill Shack: Where Roderika was once the player rests in Liurnia or gives her Chrysalids' Memento",
       },
       {
         id: "0a5107da-a70e-42ea-be73-eb3540558d37",
-        description: "Mistwood: In front of the entrance to Fort Haight",
+        description:
+          "Golden Seed - Mistwood: In front of the entrance to Fort Haight",
       },
       {
         id: "1a29d16b-0045-4b0e-ac0e-be89f01491e6",
         description:
-          "Weeping Peninsula: On a ridge along the road to Castle Morne under a phantom tree",
+          "Golden Seed - Weeping Peninsula: On a ridge along the road to Castle Morne under a phantom tree",
       },
       {
         id: "6895be25-269c-4616-b3cd-770e9a506a69",
         description:
-          "Optional: Dropped from Kenneth Haight when killed (You will ruin multiple questlines by doing this.)",
+          "Golden Seed - Optional: Dropped from Kenneth Haight when killed (You will ruin multiple questlines by doing this.)",
       }, // note: Missable once Kenneth moves to the Stormveil Throne Room as he cannot be killed there.
       {
         id: "f7b3df16-341a-4fe7-b1b0-ac8f2a76d7fe",
         description:
-          "Fringefolk Hero's Grave: Defeat the Ulcerated Tree Spirit",
+          "Golden Seed - Fringefolk Hero's Grave: Defeat the Ulcerated Tree Spirit",
+      },
+      {
+        id: "3b5ea50c-2fab-41d6-ae91-ac6d387aa7a7",
+        description: "Sacred Tear - Third Church of Marika",
+      },
+    ],
+  },
+  {
+    id: "2a4b19d7-0213-4141-b5e4-a48cda27ab20",
+    name: "Weeping Peninsula",
+    requirements: [
+      {
+        id: "2788bbbf-3fac-4880-bb5b-d0c2920b5a8b",
+        description: "Sacred Tear - Callu Baptismal Church",
+      },
+      {
+        id: "67ab2cfa-0b3b-46e8-b7f7-9f1103bd3978",
+        description: "Sacred Tear - Church of Pilgrimage",
+      },
+      {
+        id: "a15b57b9-0dcb-4647-93eb-2d5759db3fbc",
+        description: "Sacred Tear - Fourth Church of Marika",
       },
     ],
   },
@@ -52,12 +76,12 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "792e32be-886a-4d9b-8037-b786fdebc270",
         description:
-          "Secluded Cell: In the room where the player meets Nepheli",
+          "Golden Seed - Secluded Cell: In the room where the player meets Nepheli",
       },
       {
         id: "c85e4539-9400-47a7-ba64-297da641a5cb",
         description:
-          "Liftside Chamber: Defeat the Lesser Ulcerated Tree Spirit in the hidden underground area.",
+          "Golden Seed - Liftside Chamber: Defeat the Lesser Ulcerated Tree Spirit in the hidden underground area.",
       },
     ],
   },
@@ -68,26 +92,39 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "75bc7276-be24-43af-a221-6c47ac16796b",
         description:
-          "Academy Gate Town: Northwest of the broken bridge leading to the Academy's southern gate",
+          "Golden Seed - Academy Gate Town: Northwest of the broken bridge leading to the Academy's southern gate",
       },
       {
         id: "fd83f744-dc4f-43b5-a341-a757031988b6",
-        description: "Raya Lucaria Academy: West of the Academy's courtyard",
+        description:
+          "Golden Seed - Raya Lucaria Academy: West of the Academy's courtyard",
       },
       {
         id: "cb6ea1b0-b0a3-4e91-a8b0-3857f4233c8b",
         description:
-          "Caria Manor: In the manor's upper level under a phantom tree ",
+          "Golden Seed - Caria Manor: In the manor's upper level under a phantom tree ",
       },
       {
         id: "35a42e27-0f42-4681-9aea-61a21473ea6c",
         description:
-          "Main Academy Gate: At the end of the broken bridge to the north under a phantom tree",
+          "Golden Seed - Main Academy Gate: At the end of the broken bridge to the north under a phantom tree",
       },
       {
         id: "4575bb54-91e9-415a-ad76-d3285898a996",
         description:
-          "Ravine-Veiled Village: To the southwest of this grace under a phantom tree",
+          "Golden Seed - Ravine-Veiled Village: To the southwest of this grace under a phantom tree",
+      },
+      {
+        id: "1eab7945-d4fb-4108-b076-d415c168cf7d",
+        description: "Sacred Tear - Church of Irith",
+      },
+      {
+        id: "1774f563-e8fc-4a6a-b0a8-a9315c1ab875",
+        description: "Sacred Tear - Bellum Church",
+      },
+      {
+        id: "4e1a0263-85f4-4654-a34f-f4862c37a744",
+        description: "Sacred Tear - Church of Inhibition",
       },
     ],
   },
@@ -98,7 +135,7 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "100c4c3c-a115-4f9e-9391-31074d242a5a",
         description:
-          "After acquiring Moon of Nokstella, exit the temple south and take the elevator",
+          "Golden Seed - After acquiring Moon of Nokstella, exit the temple south and take the elevator",
       },
     ],
   },
@@ -109,7 +146,7 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "d8867ee8-1312-48fd-b309-ea5619ca4d17",
         description:
-          "Worshipper's Woods: To the north and slightly west of this grace, across the bridge",
+          "Golden Seed - Worshipper's Woods: To the north and slightly west of this grace, across the bridge",
       },
     ],
   },
@@ -120,27 +157,27 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "949e932f-18db-4640-a0fc-d67c6d2bcd2a",
         description:
-          "Outer Wall Phantom Tree: Two are found inside the western gate under a phantom tree ",
+          "Golden Seed - Outer Wall Phantom Tree: Two are found inside the western gate under a phantom tree ",
       },
       {
         id: "5677cb95-2358-4de1-946b-e531f1ef10bc",
         description:
-          "Capital Outskirts: Two are found in the northwestern section of the outskirts under a phantom tree",
+          "Golden Seed - Capital Outskirts: Two are found in the northwestern section of the outskirts under a phantom tree",
       },
       {
         id: "fb721bb0-8078-46dc-a04e-90a5d48eb449",
         description:
-          "Outer Wall Phantom Tree: Defeat the Lesser Ulcerated Tree Spirit south of this grace",
+          "Golden Seed - Outer Wall Phantom Tree: Defeat the Lesser Ulcerated Tree Spirit south of this grace",
       },
       {
         id: "fa1648e4-d319-498d-93b8-da29537bfa62",
         description:
-          "Avenue Balcony: Defeat the Lesser Ulcerated Tree Spirit north of this site of grace",
+          "Golden Seed - Avenue Balcony: Defeat the Lesser Ulcerated Tree Spirit north of this site of grace",
       },
       {
         id: "2f13b2dc-65f7-43ff-b969-e3c184e14ecb",
         description:
-          "West Capital Rampart: To the southeast of this grace, guarded by a gargoyle",
+          "Golden Seed - West Capital Rampart: To the southeast of this grace, guarded by a gargoyle",
       },
     ],
   },
@@ -150,21 +187,27 @@ export const GoldenSeedsList: Array<ListType> = [
     requirements: [
       {
         id: "6c2816d9-e6d5-41d4-9036-66fc65cfb762",
-        description: "Bestial Sanctum: On the path leading to the sanctum",
+        description:
+          "Golden Seed - Bestial Sanctum: On the path leading to the sanctum",
       },
       {
         id: "18d179df-7299-434e-ad9a-f814e216ff4a",
         description:
-          "Sellia, Town of Sorcery: In the northern section of town under a phantom tree",
+          "Golden Seed - Sellia, Town of Sorcery: In the northern section of town under a phantom tree",
       },
       {
         id: "7515c05e-bcc7-4d44-acef-78a9bc58ee5e",
         description:
-          "On the road to Redmane Castle, south of where the road splits",
+          "Golden Seed - On the road to Redmane Castle, south of where the road splits",
       },
       {
         id: "36795bc0-a551-471d-9bbf-c18a2a6165fb",
-        description: "War-Dead Catacombs: Defeat the Putrid Tree Spirit",
+        description:
+          "Golden Seed - War-Dead Catacombs: Defeat the Putrid Tree Spirit",
+      },
+      {
+        id: "29da2fc0-c6e1-44ca-a912-023ed23df01d",
+        description: "Sacred Tear - Church of the Plague",
       },
     ],
   },
@@ -175,26 +218,35 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "8c288a8d-abbc-4317-950b-b2887a8486ad",
         description:
-          "Seethewater River: Found exiting the channel to the north of this location under a phantom tree",
+          "Golden Seed - Seethewater River: Found exiting the channel to the north of this location under a phantom tree",
       },
       {
         id: "9eb45cf2-1cf4-47e4-9ffc-b6025f81c644",
         description:
-          "Windmill Village: Found to the southeast of this location under a phantom tree",
+          "Golden Seed - Windmill Village: Found to the southeast of this location under a phantom tree",
       },
       {
         id: "56046c30-78fc-40e3-ac92-d15bd537aee6",
         description:
-          "Altus Highway Junction: Found on the road to the north of this site of grace",
+          "Golden Seed - Altus Highway Junction: Found on the road to the north of this site of grace",
       },
       {
         id: "d3376f71-b79d-4796-907c-8901e4325d4c",
-        description: "Minor Erdtree: To the northwest of the tree",
+        description:
+          "Golden Seed - Minor Erdtree: To the northwest of the tree",
       },
       {
         id: "a95a9019-647e-4387-80ae-c99c7dc488c8",
         description:
-          "Erdtree-Gazing Hill: North of the Lux Ruins under a phantom tree",
+          "Golden Seed - Erdtree-Gazing Hill: North of the Lux Ruins under a phantom tree",
+      },
+      {
+        id: "e2869097-fecc-4ebe-beca-fa16c28498c9",
+        description: "Sacred Tear - Second Church of Marika",
+      },
+      {
+        id: "c1bc9ebf-8514-4e34-ac19-563d93a71ec0",
+        description: "Sacred Tear - Stormcaller Church",
       },
     ],
   },
@@ -205,7 +257,7 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "a95a9019-647e-4387-80ae-c99c7dc488c8",
         description:
-          "Grand Cloister: Defeat the Ulcerated Tree Spirit near the rotten waterfall at the bottom of the area",
+          "Golden Seed - Grand Cloister: Defeat the Ulcerated Tree Spirit near the rotten waterfall at the bottom of the area",
       },
     ],
   },
@@ -216,31 +268,39 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "0661fbce-198f-4018-97d7-9e3c49179f52",
         description:
-          "Minor Erdtree: Found in the valley to the northwest of this location under a phantom tree",
+          "Golden Seed - Minor Erdtree: Found in the valley to the northwest of this location under a phantom tree",
       },
       {
         id: "e3245f15-a951-4eae-ba72-362b4f327ab2",
-        description: "Flame Peak: Found before a frozen giant",
+        description: "Golden Seed - Flame Peak: Found before a frozen giant",
       },
       {
         id: "935017bf-ecc7-49db-af2d-7c3436f0b50f",
         description:
-          "Forbidden Lands: Found on the main path to the Grand Lift of Rold under a phantom tree",
+          "Golden Seed - Forbidden Lands: Found on the main path to the Grand Lift of Rold under a phantom tree",
       },
       {
         id: "5475d2c0-bf02-4060-8ea4-0d28d4946677",
         description:
-          "Ordina Liturgical Town: Found to the southwest of the town under a phantom tree",
+          "Golden Seed - Ordina Liturgical Town: Found to the southwest of the town under a phantom tree",
       },
       {
         id: "407828df-890b-4865-86ed-f011e0d46147",
         description:
-          "Giant's Mountaintop Catacombs: Defeat the Ulcerated Tree Spirit",
+          "Golden Seed - Giant's Mountaintop Catacombs: Defeat the Ulcerated Tree Spirit",
       },
       {
         id: "0e77e63e-a03a-4f13-9841-7b32f5be51c1",
         description:
-          "Consecrated Snowfield: Found to the northeast of the grace of the same name",
+          "Golden Seed - Consecrated Snowfield: Found to the northeast of the grace of the same name",
+      },
+      {
+        id: "23af881d-a08e-4b3d-8b27-d5c5e83a288c",
+        description: "Sacred Tear - Church of Repose",
+      },
+      {
+        id: "672ad2be-57b2-490b-9272-2ffc0f9f3770",
+        description: "Sacred Tear - First Church of Marika",
       },
     ],
   },
@@ -251,12 +311,12 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "fbc67b51-7626-4555-88e3-2fecd2205a42",
         description:
-          "Dragon Temple: Found in the upper area, south of the fountain plaza under a phantom tree",
+          "Golden Seed - Dragon Temple: Found in the upper area, south of the fountain plaza under a phantom tree",
       },
       {
         id: "a89db59b-9e54-4d97-bec0-c277606bb103",
         description:
-          "Dragon Temple Rooftop: To the southeast of this grace, amongst several sets of pillars under a phantom tree",
+          "Golden Seed - Dragon Temple Rooftop: To the southeast of this grace, amongst several sets of pillars under a phantom tree",
       },
     ],
   },
@@ -267,7 +327,7 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "90166dd7-7500-4324-8bd5-2b5cf90870ff",
         description:
-          "Ninth Mt. Gelmir Campsite: Found to the west of this grace under a phantom tree",
+          "Golden Seed - Ninth Mt. Gelmir Campsite: Found to the west of this grace under a phantom tree",
       },
     ],
   },
@@ -278,7 +338,7 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "603c30d7-9e17-4c56-9496-e2ef986f247a",
         description:
-          "Defeat the Ulcerated Tree Spirit found in the second lake of rot",
+          "Golden Seed - Defeat the Ulcerated Tree Spirit found in the second lake of rot",
       },
     ],
   },
@@ -289,12 +349,12 @@ export const GoldenSeedsList: Array<ListType> = [
       {
         id: "9c3e3dbe-be2b-4890-acbf-06b66cdcaafe",
         description:
-          "Blood Lake: On a central island in the south portion of the blood lake (Mutually exclusive with the seed below unless both are looted before reloading the area)",
+          "Golden Seed - Blood Lake: On a central island in the south portion of the blood lake (Mutually exclusive with the seed below unless both are looted before reloading the area)",
       },
       {
         id: "d9b79de6-b5f3-493d-bd74-5b13f7dc7e5a",
         description:
-          "Dynasty Museum Midpoint: On the rocks behind the Sanguine Noble (Mutually exclusive with the seed above unless both are looted before reloading the area)",
+          "Golden Seed - Dynasty Museum Midpoint: On the rocks behind the Sanguine Noble (Mutually exclusive with the seed above unless both are looted before reloading the area)",
       },
     ],
   },
