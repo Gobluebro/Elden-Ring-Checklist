02642d4cfccf2285ce1c1622e471dd497b17d0ad
Griffin Strayer <14102413+Gobluebro@users.noreply.github.com>
2024-05-23 18:08:16 -0400

    Add bell bearings #43

--- data/lists/miscellaneous.ts
+++ data/lists/miscellaneous.ts
@@ -197,4 +197,374 @@ export const MiscellaneousList: Array<ListType> = [
       },
     ],
   },
+  {
+    id: "4934833c-c363-4ecf-a238-cea2bccece33",
+    name: "Bell Bearings",
+    requirements: [
+      {
+        id: "d31a2e70-448c-4590-b2c2-f3593b74abea",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Kalé's+Bell+Bearing",
+          "Kalé's Bell Bearing - Limgrave - Church of Elleh"
+        ),
+      },
+      {
+        id: "5dc6a5ae-9917-4fdd-821c-0396df8825dd",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(1)",
+          "Nomadic Merchant's Bell Bearing [1] - Limgrave - East of Saintsbridge"
+        ),
+      },
+      {
+        id: "957d51f6-8bd5-49c3-b744-bf1e64b1cdb6",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(2)",
+          "Nomadic Merchant's Bell Bearing [2] - Limgrave - Found on the road between the Mistwood Ruins and Fort Haight"
+        ),
+      },
+      {
+        id: "e9176d4e-1c0a-4846-87c2-65a789c2d274",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(3)",
+          "Nomadic Merchant's Bell Bearing [3] - Limgrave - Underneath the rock structure southeast of the Coastal Cave"
+        ),
+      },
+      {
+        id: "eda59cbd-d7b3-4160-a5ac-07266faae379",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(4)",
+          "Nomadic Merchant's Bell Bearing [4] - Limgrave - Castle Morne Rampart"
+        ),
+      },
+      {
+        id: "1d2edbc0-378d-44e9-ac98-0705b3ea76e5",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(5)",
+          "Nomadic Merchant's Bell Bearing [5] - Liurnia - Liurnia Lake Shore"
+        ),
+      },
+      {
+        id: "8ed3f896-19d0-4506-98fa-eb470794b204",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(6)",
+          "Nomadic Merchant's Bell Bearing [6] - Liurnia - Under the rocky overpass just north of Bellum Church"
+        ),
+      },
+      {
+        id: "70ff91ad-0ec8-49a4-92b2-33c5e7fbab6a",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(7)",
+          "Nomadic Merchant's Bell Bearing [7] - Altus Plateau - Sitting by a fire at the side of the broken greatbridge, near the Forest-Spanning Greatbridge grace."
+        ),
+      },
+      {
+        id: "a0a69649-9fcb-46be-aeb2-ad1bb99efff8",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(8)",
+          "Nomadic Merchant's Bell Bearing [8] - Mt. Gelmir - East of the Ninth Mt. Gelmir Campsite grace, resting on a ledge halfway up the mountain"
+        ),
+      },
+      {
+        id: "829a2a40-28c6-4faa-8aa8-12af994105c2",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(9)",
+          "Nomadic Merchant's Bell Bearing [9] - Caelid - Sitting by a campfire east of the Astray from Caelid Highway North grace"
+        ),
+      },
+      {
+        id: "481a67e6-1d02-4d43-83b5-4c69aebe6be5",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Nomadic+Merchant's+Bell+Bearing+(10)",
+          "Nomadic Merchant's Bell Bearing [10] - Caelid - South-west of the Southern Aeonia Swamp Bank grace"
+        ),
+      },
+      {
+        id: "a5074289-1ba9-4c92-acb6-5c113b2baa3b",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Isolated+Merchant's+Bell+Bearing+(1)",
+          "Isolated Merchant's Bell Bearing [1] - Weeping Peninsula - Isolated Merchant's Shack"
+        ),
+      },
+      {
+        id: "9a137a44-b9f4-4aad-a46c-bb674e986a90",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Isolated+Merchant's+Bell+Bearing+(2)",
+          "Isolated Merchant's Bell Bearing [2] - Liurnia of the Lakes - Outside of the Raya Lucaria Academy"
+        ),
+      },
+      {
+        id: "afeb1ed0-eb11-4dce-93f1-7f4be93cacc4",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Isolated+Merchant's+Bell+Bearing+(3)",
+          "Isolated Merchant's Bell Bearing [3] - Dragonbarrow - Isolated Merchant's Shack"
+        ),
+      },
+      {
+        id: "2b01231a-a039-4515-8f56-5aba622a1b93",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Hermit+Merchant's+Bell+Bearing+(1)",
+          "Hermit Merchant's Bell Bearing [1] - Leyndell - Hermit Merchant's Shack"
+        ),
+      },
+      {
+        id: "9aed955f-b589-44ce-90c8-1ea74b352c7c",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Hermit+Merchant's+Bell+Bearing+(2)",
+          "Hermit Merchant's Bell Bearing [2] - Mountaintops of the Giants - South of Stargazers' Ruins beneath the cliffs"
+        ),
+      },
+      {
+        id: "d0731b26-3ff5-4a2b-86d5-89cbdd68a021",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Hermit+Merchant's+Bell+Bearing+(3)",
+          "Hermit Merchant's Bell Bearing [3] - Ainsel River - In the far end of a large stone ruin in the south-west"
+        ),
+      },
+      {
+        id: "c6bbc907-47da-4169-b983-b72ba1480d94",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Abandoned+Merchant's+Bell+Bearing",
+          "Abandoned Merchant's Bell Bearing - Siofra River - Playing music by an unlit campfire"
+        ),
+      },
+      {
+        id: "9e0d378b-7343-43ab-b741-59fc5afc6012",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Imprisoned+Merchant's+Bell+Bearing",
+          "Imprisoned Merchant's Bell Bearing - Mohgwyn Palace - Mohgwyn Dynasty Mausoleum"
+        ),
+      },
+      {
+        id: "77fbb2af-9d1b-4be3-9870-9d68b967fbe3",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Smithing-Stone+Miner's+Bell+Bearing+(1)",
+          "Smithing-Stone Miner's Bell Bearing [1] - Dropped by the Crystalian Boss at the end of Raya Lucaria Crystal Tunnel"
+        ),
+      },
+      {
+        id: "b4a895e9-6839-432f-bfc5-958c1670f43d",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Smithing-Stone+Miner's+Bell+Bearing+(2)",
+          "Smithing-Stone Miner's Bell Bearing [2] - Found in a chest at the first secret area inside the Sealed Tunnel."
+        ),
+      },
+      {
+        id: "54da7b4c-7fcd-45ec-b35a-8f9076520238",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Smithing-Stone+Miner's+Bell+Bearing+(3)",
+          "Smithing-Stone Miner's Bell Bearing [3] - Mountaintop of the Giants - Zamor Ruins"
+        ),
+      },
+      {
+        id: "32830176-3f11-47bf-8f34-ab395d4a9d70",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Smithing-Stone+Miner's+Bell+Bearing+(4)",
+          "Smithing-Stone Miner's Bell Bearing [4] - Dropped by The Godskin Duo in Crumbling Farum Azula"
+        ),
+      },
+      {
+        id: "2c6a510b-8f81-4497-afa0-a926505f111c",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Somberstone+Miner's+Bell+Bearing+(1)",
+          "Somberstone Miner's Bell Bearing [1] - Dropped by the Fallingstar Beast in the Sellia Crystal Tunnel"
+        ),
+      },
+      {
+        id: "0b097d31-8fa8-4f62-bda2-097b027b91c6",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Somberstone+Miner's+Bell+Bearing+(2)",
+          "Somberstone Miner's Bell Bearing [2] - Dropped by the Crystalian Spear and Ringblade boss in the Altus Tunnel"
+        ),
+      },
+      {
+        id: "82a4e726-ea01-4450-86ee-e6dcd10ae935",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Somberstone+Miner's+Bell+Bearing+(3)",
+          "Somberstone Miner's Bell Bearing [3] - Mountaintops of the Giants - First Church of Marika"
+        ),
+      },
+      {
+        id: "c60a3805-f605-4a93-a918-a0f5a01e7862",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Somberstone+Miner's+Bell+Bearing+(4)",
+          "Somberstone Miner's Bell Bearing [4] - Crumbling Farum Azula - Near the Tempest-Facing Balcony grace"
+        ),
+      },
+      {
+        id: "055a17ae-8019-48ca-94be-8ef95f20dbbe",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Somberstone+Miner's+Bell+Bearing+(5)",
+          "Somberstone Miner's Bell Bearing [5] - Crumbling Farum Azula - Small Church near the Great Bridge grace"
+        ),
+      },
+      {
+        id: "cc3b68db-f121-441e-9804-26785a69af57",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Glovewort+Picker's+Bell+Bearing+(1)",
+          "Glovewort Picker's Bell Bearing [1] - Dropped by Erdtree Burial Watchdog in Wyndham Catacombs"
+        ),
+      },
+      {
+        id: "6cd5999a-45a7-4623-ac59-879e2b158da7",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Glovewort+Picker's+Bell+Bearing+(2)",
+          "Glovewort Picker's Bell Bearing [2] - Dropped by Ulcerated Tree Spirit in Giants' Mountaintop Catacombs."
+        ),
+      },
+      {
+        id: "c22a1031-e22f-4b8a-b920-f6f8c7f82785",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Glovewort+Picker's+Bell+Bearing+(3)",
+          "Glovewort Picker's Bell Bearing [3] - Crumbling Farum Azula - Under a gazebo next to the lake"
+        ),
+      },
+      {
+        id: "7a48adbd-3b75-4c53-8e9e-48d909c1753b",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Ghost-Glovewort+Picker's+Bell+Bearing+(1)",
+          "Ghost-Glovewort Picker's Bell Bearing [1] - Nokron, Eternal City - Lying at the base of a gazebo"
+        ),
+      },
+      {
+        id: "902a259a-8556-4b7e-a8fb-e89612d8aef2",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Ghost-Glovewort+Picker's+Bell+Bearing+(2)",
+          "Ghost-Glovewort Picker's Bell Bearing [2] - Nokstella, Eternal City - In a chest guarded by several Silver Tears with shields in the corner of a small room"
+        ),
+      },
+      {
+        id: "ae659134-a530-49e5-80db-62bf1fa54769",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Ghost-Glovewort+Picker's+Bell+Bearing+(3)",
+          "Ghost-Glovewort Picker's Bell Bearing [3] - Elphael, Brace of the Haligtree - In a graveyard"
+        ),
+      },
+      {
+        id: "83f1d82f-ef30-4659-a771-d6d512577f55",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Bone+Peddler's+Bell+Bearing",
+          "Bone Peddler's Bell Bearing - Defeat the Bell Bearing Hunter after nightly invasion at the Warmaster's Shack"
+        ),
+      },
+      {
+        id: "09cabed0-989c-4799-8b89-fed41fc10ef5",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Meat+Peddler's+Bell+Bearing",
+          "Meat Peddler's Bell Bearing - Defeat the Bell Bearing Hunter after nightly invasion at the Church of Vows"
+        ),
+      },
+      {
+        id: "7cb6b968-e47c-46c7-ad38-3b0e9f17b27e",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Medicine+Peddler's+Bell+Bearing",
+          "Medicine Peddler's Bell Bearing - Defeat the Bell Bearing Hunter after nightly invasion at the Hermit Merchant's Shack in Altus Plateau"
+        ),
+      },
+      {
+        id: "88e8375a-bcc4-4aeb-9ea8-6b86141f0695",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Gravity+Stone+Peddler's+Bell+Bearing",
+          "Gravity Stone Peddler's Bell Bearing - Defeat the Bell Bearing Hunter after nightly invasion at the Isolated Merchant's Shack in Dragonbarrow"
+        ),
+      },
+      {
+        id: "e27b3d98-3667-456a-8189-3c337377bed7",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Sellen's+Bell+Bearing",
+          "Sellen's Bell Bearing - Looted from Sorceress Sellen's body at Witchbane Ruins"
+        ),
+      },
+      {
+        id: "c59f372c-d904-4499-8a6f-dd2d27b32ac7",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Seluvis's+Bell+Bearing",
+          "Seluvis's Bell Bearing - Looted from Seluvis' body in Seluvis's Rise after reaching the end of Ranni's questline."
+        ),
+      },
+      {
+        id: "9dd3dfd6-d23a-4fda-a069-9245243033b8",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Thops's+Bell+Bearing",
+          "Thops's Bell Bearing - Looted from Thops's body"
+        ),
+      },
+      {
+        id: "1f00493a-dee4-41ff-89f4-a10533b18355",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Corhyn's+Bell+Bearing",
+          "Corhyn's Bell Bearing - Obtained from Brother Corhyn at the end of Goldmask's questline"
+        ),
+      },
+      {
+        id: "69f28562-1468-4904-8f05-f27ae25789a9",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Miriel's+Bell+Bearing",
+          "Miriel's Bell Bearing - Looted from Miriel, Pastor of Vows' body at the Church of Vows"
+        ),
+      },
+      {
+        id: "d6f0ee64-75d4-4dc1-98d3-7043a216f907",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/D's+Bell+Bearing",
+          "D's Bell Bearing - Looted from D's body"
+        ),
+      },
+      {
+        id: "737412f6-9082-4ae3-9b2a-684fc6b5af03",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Gowry's+Bell+Bearing",
+          "Gowry's Bell Bearing - Looted from Gowry's body at Gowry's Shack in eastern Caelid"
+        ),
+      },
+      {
+        id: "f1253955-ce1e-428a-bfaf-d2de219f936f",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Rogier's+Bell+Bearing",
+          "Rogier's Bell Bearing - Looted from Rogier's body"
+        ),
+      },
+      {
+        id: "fc9f65b5-83ee-4563-af2e-74d1a16c2538",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Bernahl's+Bell+Bearing",
+          "Bernahl's Bell Bearing - Looted from Bernahl's body at the Warmaster's Shack in Limgrave"
+        ),
+      },
+      {
+        id: "be954189-4ec0-4d29-84e7-d2a4e3dc2a24",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Iji's+Bell+Bearing",
+          "Iji's Bell Bearing - Looted from Iji's body by his anvil"
+        ),
+      },
+      {
+        id: "eadc893f-475c-4fa4-ac16-613582a00b5e",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Gostoc's+Bell+Bearing",
+          "Gostoc's Bell Bearing - Looted from Gostoc's body Stormveil Castle"
+        ),
+      },
+      {
+        id: "ee19ecf6-c366-40cd-81c8-ba4ffc6c7723",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Pidia's+Bell+Bearing",
+          "Pidia's Bell Bearing - Looted from Pidia's body in the Caria Manor"
+        ),
+      },
+      {
+        id: "8087f93d-6b71-4ed7-a087-ee023b080f93",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Patches'+Bell+Bearing",
+          "Patches' Bell Bearing - Looted from Patches' body in the Murkwater Cave"
+        ),
+      },
+      {
+        id: "c507c941-2ca6-4c61-8476-e3158ceb9cf1",
+        description: rawHTMLLink(
+          "https://eldenring.wiki.fextralife.com/Blackguard's+Bell+Bearing",
+          "Blackguard's Bell Bearing - Looted from Blackguard's body at the Boilprawn Shack in Liurnia"
+        ),
+      },
+    ],
+  },
 ];
