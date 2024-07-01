3ef1f9fc10687e55aa7a8f3b40c587cb939fb6cf
Griffin Strayer <14102413+Gobluebro@users.noreply.github.com>
2024-06-30 20:53:13 -0400

    Update config for v14 specific config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  output: "export",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
